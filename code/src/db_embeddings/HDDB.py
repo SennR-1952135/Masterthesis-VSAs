78b07ff72d412be38d1dbcd4269568a179c2c983
Sennr-1952135 <senn.robyns@student.uhasselt.be>
2024-02-22 19:45:46 +0100

    update

--- code/src/db_embeddings/HDDB.py
+++ code/src/db_embeddings/HDDB.py
@@ -1,7 +1,10 @@
 from typing import Any, List, Optional, Tuple, overload, Type, Dict
-from utils import Dictionary, Codebook, normalise_partial
+from utils import Dictionary, Codebook, normalise_partial, theoretical_similarity
 from functools import partial
+from scipy import stats
 import torchhd, torch
+import numpy as np
+import math
 
 class Column:
   def __init__(self, name:str, vsa_type:str = 'MAP', elem_dim:int = 10_000, dtype:Type = str):
@@ -17,9 +20,7 @@ class Column:
   
   def __getitem__(self, key):
     return self.codebook[key]
-
-def similiraity_cutoff(dim, num_cols, vsa_type):
-  pass
+  
 
 class HDDB:
   def __init__(self, dim:int=10_000, vsa_type:str='MAP'):
@@ -32,17 +33,28 @@ class HDDB:
     self.rows: Codebook = Codebook(dim=self.dim, vsa_type=self.vsa_type)
     # self.rows: List[torchhd.VSATensor] = []
 
+  def similiraity_cutoff(self, certainty:float = 0.9):
+    if self.vsa_type != 'BSC':
+      raise NotImplementedError
+    amount_bundled = len(self.columns)
+    exp_sim = theoretical_similarity(amount_bundled, self.vsa_type)
+    var = exp_sim * (1-exp_sim) * (1/self.dim)
+    c = exp_sim + stats.norm.ppf(1-certainty) * math.sqrt(var)
+    return c
+
   def set_columns(self, columns: Dict[str, Column] | List[str]): #TODO: typing for ints?
     if isinstance(columns[0], str):
       columns = {col: Column(col, vsa_type=self.vsa_type, elem_dim=self.dim) for col in columns}
     self.columns = columns
 
   def add_row(self, row_key: str, values: List[Any]):
-    row = torchhd.empty(1, self.dim, vsa=self.vsa_type)[0]
+    row_vectors = torchhd.empty(len(self.columns), self.dim, vsa=self.vsa_type)
     for i, col in enumerate(self.columns.values()):
-      col_value = col.codebook.add_value(values[i])
+      col_value = col.add_value(values[i])
       col_key_value_pair = torchhd.bind(col.atomic_vector, col_value)
-      row = torchhd.bundle(row, col_key_value_pair)
+      row_vectors[i] = col_key_value_pair
+      # row = torchhd.bundle(row, col_key_value_pair)
+    row = torchhd.multiset(row_vectors)
     self.rows.add_value(row_key, self.normalise(row))
 
   def most_similar_rows(self, row: torchhd.VSATensor, topk: int = 1):
@@ -50,5 +62,3 @@ class HDDB:
       
   def __getitem__(self, key):
     return self.rows[key]
-  
-      
--- code/src/db_embeddings/utils.py
+++ code/src/db_embeddings/utils.py
@@ -1,6 +1,9 @@
 import torchhd, torch, torch.nn.functional as F
 from typing import Any, List, Dict, Optional, Tuple, overload, Type
 from functools import partial
+import math
+from scipy import stats
+
 
 def norm_hamming_similarity(x, y):
     normalisation_factor = x.shape[1] if x.ndim == 2 else x.shape[0]
@@ -22,6 +25,19 @@ def normalise_partial(vsa_type, x):
     else:
         return x
     
+def theoretical_similarity(amount_bundled, vsa_type):
+    if vsa_type != 'BSC':
+      return 0
+    amount_bundled = amount_bundled + 1 if amount_bundled % 2 == 0 else amount_bundled # +1 because we need an odd amount of elements
+    expected_similarity = 1/2 + (math.comb(amount_bundled-1, int((amount_bundled-1)/2)) / 2**amount_bundled)
+    return expected_similarity
+
+def similarity_cutoff(bundle_size, dim, certainty:float = 0.9):
+  exp_sim = theoretical_similarity(bundle_size, 'BSC')
+  var = exp_sim * (1-exp_sim) * (1/dim)
+  cutoff = exp_sim + stats.norm.ppf(1-certainty) * math.sqrt(var)
+  return cutoff
+
 class Dictionary:
     def __init__(self):
         self.word2idx: Dict[str, int]  = {}
