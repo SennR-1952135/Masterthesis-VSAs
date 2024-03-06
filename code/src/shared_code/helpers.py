231b40c49d2004bf3bfa0d7da3fabdaaaf9fa510
Sennr-1952135 <senn.robyns@student.uhasselt.be>
2024-03-06 14:45:14 +0100

    Add new functions for expected accuracy and probability of error

--- code/src/shared_code/helpers.py
+++ code/src/shared_code/helpers.py
@@ -2,7 +2,7 @@ import torchhd
 import torch
 import torch.nn.functional as F
 import math
-from scipy import stats
+from scipy import stats, integrate
 from functools import partial
 
 def norm_hamming_similarity(x, y):
@@ -57,3 +57,62 @@ def top_k_vectors(reference_vector, vectors, topk=1, vsa_type='BSC'):
     sim = similarity_func(reference_vector, vectors)
     topk_sim, topk_idx = torch.topk(sim, topk, -1)
     return topk_sim, topk_idx
+
+
+def exp_acc(num_bundled, item_memory_size, dim, vsa_type):
+    if vsa_type != 'BSC' and vsa_type != 'MAP':
+      return 0
+    if vsa_type == 'BSC':
+       mu_r = 1/2
+       sigma_r = math.sqrt(mu_r*(1-mu_r)/dim)
+       mu_h = theoretical_similarity(num_bundled, vsa_type)
+       sigma_h = math.sqrt(mu_h*(1-mu_h)/dim)
+       return expected_accuracy(mu_h, sigma_h, mu_r, sigma_r, item_memory_size)
+    else:
+       return 0
+    
+def prob_corr(num_bundled, item_memory_size, dim, vsa_type):
+    if vsa_type != 'BSC' and vsa_type != 'MAP':
+      return 0
+    if vsa_type == 'BSC':
+       mu_r = 1/2
+       var_r = mu_r*(1-mu_r)/dim
+       mu_h = theoretical_similarity(num_bundled, vsa_type)
+       var_h = mu_h*(1-mu_h)/dim
+       loc = (mu_h - mu_r)
+       scale = math.sqrt(var_h + var_r)
+       p = 1-stats.norm.cdf(0, loc=loc, scale=scale)
+       return p**num_bundled
+    else:
+       return 0
+def expected_accuracy(mu_h, sigma_h, mu_r, sigma_r, N):
+  """
+  Calculates the expected accuracy (Pcorr) of retrieving the correct atomic HV.
+
+  Args:
+    mu_eta: Mean of the noise for the HV signal.
+    sigma_eta: Standard deviation of the noise for the HV signal.
+    mu_r: Mean of the reference HV signal.
+    sigma_r: Standard deviation of the reference HV signal.
+    N: Size of the item memory.
+
+  Returns:
+    The expected accuracy (Pcorr).
+  """
+
+  def integrand(x):
+    return (1 / (math.sqrt(2 * math.pi) * sigma_h)) * math.exp(-(1/2)*((x - (mu_h - mu_r)) / sigma_h)**2) * (stats.norm.cdf(x, 0, sigma_r)**(N - 1))
+
+  # Calculate the integral using numerical integration
+  integral, _ = integrate.quad(integrand, -math.inf, math.inf)
+
+  return integral
+
+def prob_of_error(num_bundled, item_memory_size, dim, vsa_type):
+    if vsa_type != 'BSC' and vsa_type != 'MAP':
+      return 0
+
+    return (1 - (1/2) * math.e**(-(num_bundled)**2/4))**(dim-1)
+    # return (item_memory_size-num_bundled)* stats.norm.cdf(math.sqrt(dim/(2*num_bundled-1)), loc=0, scale=1)
+    # return (1-(1-stats.norm.cdf(math.sqrt(dim/(2*num_bundled-1)), loc=0, scale=1))/2)**(item_memory_size-num_bundled)
+    # return (stats.norm.cdf(math.sqrt(dim/(2*num_bundled-1)), loc=0, scale=1))**((item_memory_size-num_bundled)*num_bundled)
